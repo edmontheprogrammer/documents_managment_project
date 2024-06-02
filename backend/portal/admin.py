4df91f0dfd960051637cab2c805df078df485648
Edmon Sebit <edmontheprogrammer@gmail.com>
2024-06-02 16:18:20 -0600

    Adding TelecommunicationsForm, testing model.ImageField() and model.FileField()

--- backend/portal/admin.py
+++ backend/portal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Telecommunications, GovernmentID
+
+
+admin.site.register(Telecommunications)
+admin.site.register(GovernmentID)
--- backend/portal/views.py
+++ backend/portal/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render
 
+from .forms import TelecommunicationsForm
+
 
 def home(request):
     return render(request, 'home.html', {})
@@ -15,9 +17,17 @@ def contact(request):
     return render(request, 'contact.html', {})
 
 
-@login_required
+# @login_required
 def openAccountFormPage(request):
-    return render(request, 'open-account.html', {})
+    if request.method == "POST":
+        filled_form = TelecommunicationsForm(request.POST, request.FILES)
+        if filled_form.is_valid():
+            note = "Thanks for completing the application form for opening an account with us. Our team is processing your request! DMP Team"
+            new_form = TelecommunicationsForm()
+            return render(request, "open-account.html", {"telecommunicationsform": new_form, 'note': note})
+    else:
+        form = TelecommunicationsForm()
+        return render(request, 'open-account.html', {'telecommunicationsform': form})
 
 
 @login_required
